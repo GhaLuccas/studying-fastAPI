e46c947a967a979b5ce93a407abddf49cf65d292
GhaLuccas <gitsgab@gmail.com>
2024-12-23 15:49:16 -0300

    a

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ app = FastAPI()
 async def read_root():
     data : dict[str,str] = {"massege":"Hello word"}
     return data
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id:int):
+   data : dict[str , int] = {'massage': item_id}
+   return data  
+
+
+
+@app.get('/accounst/me')
+async def me():
+    data : dict[str , str] = {"username":"userdata"}
+    return data
